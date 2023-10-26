32a6443ba07bbb1f76d48cd9264cb13d4d2be39c
jakovse <jakakovse@gmail.com>
2023-10-26 14:53:17 +0200

    Update main_page_functions.js

--- js/main_page_functions.js
+++ js/main_page_functions.js
@@ -1,4 +1,4 @@
-const filePath = "http://localhost:8081/eegRecordings/"
+const filePath = "https://github.com/jakovse/braintrip-fairdata_web_page/tree/master/eegRecordings/"
 const selectedRows = new Array(500).fill(false);
 
 function runFilter() {
